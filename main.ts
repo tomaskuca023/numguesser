4c9673dea996fb7135c767f2cb15fba5f43b20ed
tomaskuca023 <146720075+tomaskuca023@users.noreply.github.com>
2023-12-11 13:35:50 +0100

    prototype01

--- main.ts
+++ main.ts
@@ -1,35 +1,63 @@
 let minValue: number = 1
 let maxValue: number = 99
+let selection: boolean = true
 
-let minSelectValue: number = minValue
-let maxSelectValue: number = maxValue 
+let maxSelectValue: number = maxValue
+
+let minPickValue: number = minValue
+let maxPickValue: number = maxSelectValue
+
+
 
 
 let value: number = 50
 
 basic.forever(function() {
-    if (minSelectValue)
+
+    if (selection) {
+        whaleysans.showNumber(maxSelectValue)
+    }
 
 
+    if (!selection) {
+        whaleysans.showNumber(value)
+    }
 
-
-
-    whaleysans.showNumber(value)
     basic.pause(20)
 })
 
-input.onButtonPressed(Button.A, function() {
-    maxSelectValue = value
-    value = randint(minSelectValue, maxSelectValue)
-})
+//selection screen
+if (selection) {
+    input.onButtonPressed(Button.A, function() {
+        maxSelectValue -= 1
+    })
 
-input.onButtonPressed(Button.B, function () {
-    minSelectValue = value
-    value = randint(minSelectValue, maxSelectValue)
-})
+    input.onButtonPressed(Button.B, function () {
+        maxSelectValue += 1
+    })
+    input.onButtonPressed(Button.AB, function () {
+        selection = false
+    })
+}
 
-input.onButtonPressed(Button.AB, function () {
-    minSelectValue = minValue
-    maxSelectValue = maxValue
-    value = 50
-})
+
+
+//game
+if (!selection) {
+    input.onButtonPressed(Button.A, function() {
+      maxPickValue = value
+       value = Math.round(randint(minPickValue, maxPickValue))
+    })
+
+    input.onButtonPressed(Button.B, function () {
+        minPickValue = value
+        value = Math.round(randint(minPickValue, maxPickValue))
+    })
+
+    input.onButtonPressed(Button.AB, function () {
+       minPickValue = minValue
+       maxPickValue = maxSelectValue
+       value = 50
+       selection = true
+    })
+}
